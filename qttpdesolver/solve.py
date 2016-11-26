87b2b3e679818a4041885c03389e039fb015078b
Andrei Chertkov <andrey.mipt@mail.ru>
2016-11-26 17:49:42 +0300

    More compact realization of solve module

--- qttpdesolver/solve.py
+++ qttpdesolver/solve.py
@@ -1,192 +1,100 @@
 # -*- coding: utf-8 -*-
 import time
+from numpy.linalg import norm
 
-import tt.multifuncrs2 as multifuncrs2
-
-from utils.general import MODE_TT, ttround, vsum, rel_err, vprod
-from utils.grid import quan_on_grid
-
-from solvers.solver_fs import SolverFs
-from solvers.solver_fd import SolverFd
+from solvers.solver import create_solver
+from tensor_wrapper.vector import Vector
 
 def auto_solve(PDE, PDESolver=None, present_res_1s=True, return_solver=False):
     '''
     This function solve PDE, construct derivatives of the solution and some
-    other quantities, and compare the result with the analytical values.
+    other quantities, and compare the results with the analytical values.
                     Input
     PDE             - instance of the PDE class with equation parameters
-                      and parameters of the calculation
-    PDESolver       - [None] solver class instance (if is None, then solver
-                      is selected according to PDE.solver_txt value)
+                      and the calculation settings
+    PDESolver       - [None] solver class instance (if is None, then solver is
+                      selected according to PDE.solver_txt and PDE.dim values)
     present_res_1s  - [True] if is True, then calculation result will be
                       presented in a one string mode
     return_solver   - [False] if is True, then solver class instance 
                       will be returned.
-                    Output (if return_solver==True)
-    PDESolver       - solver class instance 
+                    Output 
+    PDESolver       - solver class instance (if return_solver==True)
     '''
     PDE.clean()
-
-    if PDESolver is not None:
-        pass
-    elif PDE.solver_txt=='fs':
-        PDESolver = SolverFs(PDE)
-    elif PDE.solver_txt=='fd':
-        PDESolver = SolverFd(PDE)
-    else:
-        raise ValueError('Unknown solver type.')
-        
-    PDESolver.generate_matrices()
-    PDESolver.generate_system()
+    
+    if PDESolver is None:
+        PDESolver = create_solver(PDE)   
     PDESolver.solve()
     
     _t = time.time()
-    construct_solution(PDE)
-    construct_derivative(PDE)
-    if PDE.with_en:
-        construct_energy(PDE) 
-    construct_uf(PDE, PDESolver)
-    construct_uu(PDE)
-    PDE.t['prepare_res'] = time.time() - _t
+    prep_solution(PDE)
+    prep_derivative(PDE)
+    prep_uu(PDE)
+    prep_uf(PDE, PDESolver)
+    PDE.t['prep'] = time.time() - _t
     if PDE.verb_gen: 
-        PDE._present('Time of additional calcs.  : %-8.4f'%PDE.t['prepare_res']) 
- 
+        PDE._present('Time of additional calcs.  : %-8.4f'%PDE.t['prep']) 
+        
     if present_res_1s:
         PDE.present_res_1s()    
+        
     if return_solver:
         return PDESolver
         
-def construct_solution(PDE):
+def prep_solution(PDE):
     '''
-    Function construct analytical solution (if it is known) of the PDE
+    Function construct analytical solution of the PDE (if it is known) 
     and calculate relative error of the calculation result.
     '''
-    _t = time.time()
-    if PDE.u is None:
-        PDE.u_real = None
-    else:
-        PDE.u_real = quan_on_grid(PDE.u_func, PDE.d, PDE.dim, 
-                                  PDE.tau['u_real']['round'],
-                                  PDE.tau['u_real']['cross'],
-                                  PDE.mode, 'rc', 'u', PDE.verb_cross)                         
-    if PDE.u_real is not None and PDE.mode==MODE_TT:
-        PDE.u_real_ranks, PDE.u_real_erank = PDE.u_real.r, PDE.u_real.erank
-    PDE.t['u_real'] = time.time() - _t
-        
-    PDE.u_err = rel_err(PDE.u_real, PDE.u_calc)
-    
-def construct_derivative(PDE):
-    '''
-    Function construct analytical derivatives of the solution (if it is known) 
-    of the PDE and calculate relative error of the calculation result.
-    '''
-    _t = time.time()
-    PDE.ud_real = [None]*PDE.dim
-    for i in range(PDE.dim):
-        if [PDE.ux, PDE.uy, PDE.uz][i] is None:
-            PDE.ud_real = None
-            break
-        ud_func = [PDE.ux_func, PDE.uy_func, PDE.uz_func][i]
-        PDE.ud_real[i] = quan_on_grid(ud_func, PDE.d, PDE.dim, 
-                                      PDE.tau['ud_real']['round'], 
-                                      PDE.tau['ud_real']['cross'], 
-                                      mode=PDE.mode, grid='cc', verb=False)
-    PDE.t['ud_real'] = time.time() - _t
-    
-    if PDE.ud_real is not None and PDE.ud_calc is not None:
-        PDE.ud_err = [rel_err(PDE.ud_real[i], PDE.ud_calc[i]) for i in range(PDE.dim)]
-        
-def construct_energy(PDE): 
-    '''
-    Function construct analytical energy (if analytical derivatives are known) 
-    of the PDE and calculate relative error of the calculation result.
-    '''
-    _t = time.time()
-    if PDE.ud_calc is None and PDE.ud_real is None:
-        PDE.t['en'] = time.time()-_t
-        return
-        
-    D = [None]*PDE.dim
-    if PDE.dim>=1:
-        D[0] = quan_on_grid(PDE.k_func, PDE.d, PDE.dim, 
-                            PDE.tau['en_real']['round'], PDE.tau['en_real']['cross'],
-                            PDE.mode, 'uxe', 'Dx for e', PDE.verb_cross)
-    if PDE.dim>=2:
-        D[1] = quan_on_grid(PDE.k_func, PDE.d, PDE.dim, 
-                            PDE.tau['en_real']['round'], PDE.tau['en_real']['cross'],
-                            PDE.mode, 'uye', 'Dy for e', PDE.verb_cross)
-    if PDE.dim>=3:
-        D[2] = quan_on_grid(PDE.k_func, PDE.d, PDE.dim, 
-                            PDE.tau['en_real']['round'], PDE.tau['en_real']['cross'],
-                            PDE.mode, 'uze', 'Dz for e', PDE.verb_cross)
-                            
-    def calc_en(ud, K, tau_cross, tau_round, mode=MODE_TT):
-        if mode != MODE_TT:
-            e = ud*K*ud
-        else:
-            e = multifuncrs2([ud, K, ud], lambda x: x[:, 0]*x[:, 1]*x[:, 2], 
-                             tau_cross, verb=False)
-        e = ttround(e, tau_round)
-        e = vsum(e, tau_round)
-        return e 
+    if PDE.u is not None:
+        r = [PDE.GRD.xr, PDE.GRD.yr, PDE.GRD.zr][:PDE.dim]
+        PDE.u_real = Vector.func(r, PDE.u_func, None, PDE.verb_crs, 'u_real')                        
+        PDE.u_err = PDE.u_real.rel_err(PDE.u_calc)
+        PDE.u_real_ranks = PDE.u_real.r
+        PDE.r['u_real'] = PDE.u_real.erank
 
-    if PDE.ud_real is not None:
-        PDE.en_real = 0.
-        for i in range(PDE.dim):
-            PDE.en_real+= calc_en(PDE.ud_real[i], D[i], 
-                                  PDE.tau['en_real']['cross'], 
-                                  PDE.tau['en_real']['round'], PDE.mode)
-        PDE.en_real*= (PDE.h**PDE.dim)
-
-    if PDE.ud_calc is not None:
-        PDE.en_calc = 0.
-        for i in range(PDE.dim):
-            PDE.en_calc+= calc_en(PDE.ud_calc[i], D[i],
-                                  PDE.tau['en_calc']['cross'], 
-                                  PDE.tau['en_calc']['round'], PDE.mode)
-        PDE.en_calc*= (PDE.h**PDE.dim)
-
-    PDE.en_err = rel_err(PDE.en_real, PDE.en_calc)
-    PDE.t['en'] = time.time()-_t
-    
-def construct_uf(PDE, PDESolver): 
+def prep_derivative(PDE):
     '''
-    Function construct (u, f) product for calculation result u_calc, and for
-    analytical solution u_real (if known), and calculate the relative error.
+    Function construct analytical derivatives of the solution of the PDE 
+    (if it is known) and calculate relative errors of the calculation results.
     '''
-    tau_round = PDE.tau['en_calc']['round']
-    
-    _t = time.time()
-    if PDE.u_calc is not None:
-        uf = vprod([PDE.u_calc, PDESolver.F], tau_round)
-        PDE.uf_calc = vsum(uf, tau_round) * (PDE.h**PDE.dim)
-    PDE.t['uf_calc'] = time.time()-_t
-
-    _t = time.time()
-    if PDE.u_real is not None:
-        uf = vprod([PDE.u_real, PDESolver.F], tau_round)
-        PDE.uf_real = vsum(uf, tau_round) * (PDE.h**PDE.dim)
-    PDE.t['uf_real'] = time.time()-_t
-
-    PDE.uf_err = rel_err(PDE.uf_real, PDE.uf_calc)
-    
-def construct_uu(PDE): 
+    if PDE.ux is not None:
+        r = [PDE.GRD.xc, PDE.GRD.yr, PDE.GRD.zr][:PDE.dim]
+        PDE.ux_real = Vector.func(r, PDE.ux_func, None, PDE.verb_crs, 'ux_real')                        
+        PDE.ux_err = PDE.ux_real.rel_err(PDE.ux_calc)
+        PDE.r['ux_real'] = PDE.ux_real.erank
+    if PDE.uy is not None:
+        r = [PDE.GRD.xr, PDE.GRD.yc, PDE.GRD.zr][:PDE.dim]
+        PDE.uy_real = Vector.func(r, PDE.uy_func, None, PDE.verb_crs, 'uy_real')                        
+        PDE.uy_err = PDE.uy_real.rel_err(PDE.uy_calc)
+        PDE.r['uy_real'] = PDE.uy_real.erank
+    if PDE.uz is not None:
+        r = [PDE.GRD.xr, PDE.GRD.yr, PDE.GRD.zc][:PDE.dim]
+        PDE.uz_real = Vector.func(r, PDE.uz_func, None, PDE.verb_crs, 'uz_real')                        
+        PDE.uz_err = PDE.uz_real.rel_err(PDE.uz_calc)
+        PDE.r['uz_real'] = PDE.uz_real.erank
+          
+def prep_uu(PDE): 
     '''
     Function construct (u, u) product for calculation result u_calc, and for
     analytical solution u_real (if known), and calculate the relative error.
     '''
-    tau_round = PDE.tau['en_calc']['round']
-    
-    _t = time.time()
-    if PDE.u_calc is not None:
-        uu = vprod([PDE.u_calc, PDE.u_calc], tau_round)
-        PDE.uu_calc = vsum(uu, tau_round) * (PDE.h**PDE.dim)
-    PDE.t['uu_calc'] = time.time()-_t
-
-    _t = time.time()
-    if PDE.u_real is not None:
-        uu = vprod([PDE.u_real, PDE.u_real], tau_round)
-        PDE.uu_real = vsum(uu, tau_round) * (PDE.h**PDE.dim)
-    PDE.t['uu_real'] = time.time()-_t
-
-    PDE.uu_err = rel_err(PDE.uu_real, PDE.uu_calc)
+    if PDE.u_calc is not None and PDE.u_calc.isnotnone:
+        PDE.uu_calc = (PDE.u_calc * PDE.u_calc).sum() * (PDE.h**PDE.dim) 
+    if PDE.u_real is not None and PDE.u_real.isnotnone:
+        PDE.uu_real = (PDE.u_real * PDE.u_real).sum() * (PDE.h**PDE.dim)
+    if PDE.uu_calc is not None and PDE.uu_real is not None:
+        PDE.uu_err = norm(PDE.uu_real-PDE.uu_calc)/norm(PDE.uu_real)
+        
+def prep_uf(PDE, PDESolver): 
+    '''
+    Function construct (u, f) product for calculation result u_calc, and for
+    analytical solution u_real (if known), and calculate the relative error.
+    '''
+    if PDE.u_calc is not None and PDE.u_calc.isnotnone:
+        PDE.uf_calc = (PDE.u_calc * PDESolver.f).sum() * (PDE.h**PDE.dim) 
+    if PDE.u_real is not None and PDE.u_real.isnotnone:
+        PDE.uf_real = (PDE.u_real * PDESolver.f).sum() * (PDE.h**PDE.dim)
+    if PDE.uf_calc is not None and PDE.uf_real is not None:
+        PDE.uf_err = norm(PDE.uf_real-PDE.uf_calc)/norm(PDE.uf_real)
